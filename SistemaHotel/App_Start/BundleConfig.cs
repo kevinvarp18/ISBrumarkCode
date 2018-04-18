409dae8cc7a56940a5ee81d18098f6011cf53968
kevinvarp18 <31193307+kevinvarp18@users.noreply.github.com>
2018-04-18 14:22:00 -0600

    asd

---
+++ SistemaHotel/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SistemaHotel {
+    public class BundleConfig {
+
+        public static void RegisterBundles(BundleCollection bundles) {
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery.flexisel.js",
+                        "~/Scripts/jquery-3.3.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/style.css"));
+        }
+    }
+}
---
+++ SistemaHotel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SistemaHotel.MvcApplication" Language="C#" %>
