e476b94ea3035cb73dbd50f04872577df759d841
Strik15 <31193313+Strik15@users.noreply.github.com>
2018-04-18 16:56:37 -0600

    Agrega la vista de reservas y arregla el logo

---
+++ SistemaHotel/Controllers/ReservaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaHotel.Controllers
+{
+    public class ReservaController : Controller
+    {
+        // GET: Reserva
+        public ActionResult Reserva()
+        {
+            return View();
+        }
+    }
+}
