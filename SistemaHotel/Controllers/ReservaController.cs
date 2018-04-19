689890528be0aa7e06f201facb4f3e7050ea52c7
mcalvo96 <31193305+mcalvo96@users.noreply.github.com>
2018-04-18 22:52:46 -0600

    cambios en reserva

--- SistemaHotel/Controllers/ReservaController.cs
+++ SistemaHotel/Controllers/ReservaController.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using SistemaHotel.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
+
 
 namespace SistemaHotel.Controllers
 {
     public class ReservaController : Controller
     {
+        String connectionString = WebConfigurationManager.ConnectionStrings["Sunset_Hotel"].ToString();
         // GET: Reserva
         public ActionResult Reserva()
         {
+            return View();
+        }
+
+        public ActionResult RealizarReserva(string nombreReserva, string apellidoReserva, string correoReserva, string tarjetaReserva)
+        {
+            ReservaModel rm = new ReservaModel(connectionString);
+
+            bool resultado = rm.RealizarReserva(nombreReserva,apellidoReserva,correoReserva,tarjetaReserva);
+
             return View();
         }
     }
---
+++ SistemaHotel/Models/ReservaModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace SistemaHotel.Models
+{
+    public class ReservaModel
+    {
+        private String connString;
+
+        public ReservaModel(String connString)
+        {
+            this.connString = connString;
+        }//Fin del constructor.
+
+        public bool RealizarReserva(string nombreReserva, string apellidoReserva, string correoReserva, string tarjetaReserva)
+        {
+            SqlConnection connection = new SqlConnection(this.connString);
+            String sqlStoredProcedure = "sp_realizarReserva";
+            SqlCommand cmdInsertar = new SqlCommand(sqlStoredProcedure, connection);
+            cmdInsertar.CommandType = System.Data.CommandType.StoredProcedure;
+            cmdInsertar.Parameters.Add(new SqlParameter("@nombre", nombreReserva));
+            cmdInsertar.Parameters.Add(new SqlParameter("@apellidos", apellidoReserva));
+            cmdInsertar.Parameters.Add(new SqlParameter("@correo", correoReserva));
+            cmdInsertar.Parameters.Add(new SqlParameter("@tarjeta", tarjetaReserva));
+            //cmdInsertar.Parameters.Add(new SqlParameter("@encargado ", estudiante.Encargado));
+            cmdInsertar.Connection.Open();
+            cmdInsertar.ExecuteNonQuery();
+            cmdInsertar.Connection.Close();
+
+            return true;
+        }
+    }
+}
