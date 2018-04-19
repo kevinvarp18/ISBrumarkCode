d687146941018eff96eac3d28981b7d06bb98ae5
kevinvarp18 <31193307+kevinvarp18@users.noreply.github.com>
2018-04-19 09:38:32 -0600

    asd

--- SistemaHotel/Controllers/ReservaController.cs
+++ SistemaHotel/Controllers/ReservaController.cs
@@ -7,18 +7,14 @@ using System.Web.Configuration;
 using System.Web.Mvc;
 
 
-namespace SistemaHotel.Controllers
-{
-    public class ReservaController : Controller
-    {
+namespace SistemaHotel.Controllers { 
+    public class ReservaController : Controller {
         String connectionString = WebConfigurationManager.ConnectionStrings["Sunset_Hotel"].ToString();
         // GET: Reserva
         [HttpGet]
-        public ActionResult Reserva(int idHabitacion)
-        {
+        public ActionResult Reserva(int idHabitacion) {
             ReservaModel rm = new ReservaModel(connectionString);
             List<string> habitacion = rm.obtenerDatosHabitacion(idHabitacion);
-
 
             //for (int i = 0; i < 4; i++) {
             //    ViewBag.Message += habitacion.ElementAt(i).ToString() + " ";
@@ -28,27 +24,20 @@ namespace SistemaHotel.Controllers
             ViewData["descripcion"] = habitacion.ElementAt(1).ToString();
             ViewData["imagen"] = habitacion.ElementAt(2).ToString();
             ViewData["tarifa"] = habitacion.ElementAt(3).ToString();
-
-
             return View();
         }
             
         [HttpPost]
-        public ActionResult Reserva(string nombreReserva, string apellidoReserva, string correoReserva, string tarjetaReserva)
-        {
+        public ActionResult Reserva(string nombreReserva, string apellidoReserva, string correoReserva, string tarjetaReserva) {
             ReservaModel rm = new ReservaModel(connectionString);
 
             bool resultado = rm.RealizarReserva(nombreReserva,apellidoReserva,correoReserva,tarjetaReserva);
 
-            if (resultado)
-            {
+            if (resultado) { 
                 ViewBag.Message = "La reserva se realizó exitosamente";
-            }
-            else {
+            }else{
                 ViewBag.Message = "La reserva no pudo realizar";
             }
-            
-
             return View(resultado);
         }
     }
