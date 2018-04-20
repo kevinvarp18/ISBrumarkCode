b8c7d1ff3a8a65a6f3698ee8c0a3d525218701bf
kevinvarp18 <31193307+kevinvarp18@users.noreply.github.com>
2018-04-20 17:51:26 -0600

    asdf

--- SistemaHotel/Controllers/ReservaController.cs
+++ SistemaHotel/Controllers/ReservaController.cs
@@ -1,4 +1,5 @@
-﻿using SistemaHotel.Models;
+﻿using SistemaHotel.Domain;
+using SistemaHotel.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,55 +8,59 @@ using System.Web.Configuration;
 using System.Web.Mvc;
 
 
-namespace SistemaHotel.Controllers { 
-    public class ReservaController : Controller {
+namespace SistemaHotel.Controllers
+{
+    public class ReservaController : Controller
+    {
         String connectionString = WebConfigurationManager.ConnectionStrings["Sunset_Hotel"].ToString();
-        string numero;
-        
-        public ActionResult HabitacionDisponible() {
-            return View();
-        }
+        string numeroHabitacion = "";
 
-        public ActionResult ResultadoReserva()
+        public ActionResult HabitacionDisponible()
         {
-            return View();
-        }
+            ReservaModel reservaModel = new ReservaModel(connectionString);
+            List<TipoHabitacion> tiposHabitacion = reservaModel.obtenerTiposHabitacion();
+            return View(tiposHabitacion);
+        }//Fin de la funcion HabitacionDisponible
 
         [HttpPost]
-        public ActionResult HabitacionDisponible(DateTime fechaLlegada, DateTime fechaSalida, int idTipoHabitacion) {
-            return View();
-        }
-        
-        // GET: Reserva
-        [HttpGet]
-        public ActionResult Reserva(int idHabitacion) {
-            ReservaModel rm = new ReservaModel(connectionString);
-            List<string> habitacion = rm.obtenerDatosHabitacion(idHabitacion);
+        public ActionResult HabitacionDisponible(DateTime fechaLlegada, DateTime fechaSalida, int tipoHabitacion)
+        {
+            ReservaModel reservaModel = new ReservaModel(connectionString);
+            List<Habitacion> habitacionesDisponibles = reservaModel.consultarDisponibilidad(tipoHabitacion);
+            if (habitacionesDisponibles.Count > 0)
+                return RedirectToAction("Reserva", "Reserva", new { idHabitacion = habitacionesDisponibles.First().Id, fechaLlegada = fechaLlegada, fechaSalida = fechaSalida });
+            else
+                return RedirectToAction("ResultadoReserva", "Reserva");
+        }//Fin de la función HabitacionDisponible.
 
-            //for (int i = 0; i < 4; i++) {
-            //    ViewBag.Message += habitacion.ElementAt(i).ToString() + " ";
-            //}
-            
+        public ActionResult Reserva(int idHabitacion, DateTime fechaLlegada, DateTime fechaSalida)
+        {
+            ReservaModel reservaModel = new ReservaModel(connectionString);
+            List<string> habitacion = reservaModel.obtenerDatosHabitacion(idHabitacion);
+
             ViewData["numero"] = habitacion.ElementAt(0).ToString();
             ViewData["descripcion"] = habitacion.ElementAt(1).ToString();
             ViewData["imagen"] = habitacion.ElementAt(2).ToString();
             ViewData["tarifa"] = habitacion.ElementAt(3).ToString();
-
-            numero = habitacion.ElementAt(0).ToString();                        
+            numeroHabitacion = habitacion.ElementAt(0).ToString();
 
             return View();
-        }
-            
+        }//Fin de la función Reserva.
+
         [HttpPost]
-        public ActionResult Reserva(string nombreReserva, string apellidoReserva, string correoReserva, string tarjetaReserva) {
+        public ActionResult Reserva(string nombreReserva, string apellidoReserva, string correoReserva, string tarjetaReserva)
+        {
             ReservaModel rm = new ReservaModel(connectionString);
 
             bool resultado = rm.RealizarReserva(nombreReserva, apellidoReserva, correoReserva, tarjetaReserva, 1);
             //bool resultado = rm.RealizarReserva(nombreReserva,apellidoReserva,correoReserva,tarjetaReserva,int.Parse(this.numero));
 
-            if (resultado) { 
+            if (resultado)
+            {
                 ViewBag.Message = "La reserva se realizó exitosamente";
-            }else{
+            }
+            else
+            {
                 ViewBag.Message = "La reserva no pudo realizar";
             }
             return View(resultado);
--- SistemaHotel/Models/ReservaModel.cs
+++ SistemaHotel/Models/ReservaModel.cs
@@ -4,17 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
 using System.Data;
+using SistemaHotel.Domain;
 
-
-namespace SistemaHotel.Models {
-    public class ReservaModel {
+namespace SistemaHotel.Models
+{
+    public class ReservaModel
+    {
         private String connString;
 
-        public ReservaModel(String connString) {
+        public ReservaModel(String connString)
+        {
             this.connString = connString;
         }//Fin del constructor.
 
-        public bool RealizarReserva(string nombreReserva, string apellidoReserva, string correoReserva, string tarjetaReserva,int numero) {
+        public bool RealizarReserva(string nombreReserva, string apellidoReserva, string correoReserva, string tarjetaReserva, int numero)
+        {
             SqlConnection connection = new SqlConnection(this.connString);
             String sqlStoredProcedure = "sp_realizarReserva";
             SqlCommand cmdInsertar = new SqlCommand(sqlStoredProcedure, connection);
@@ -23,16 +27,15 @@ namespace SistemaHotel.Models {
             cmdInsertar.Parameters.Add(new SqlParameter("@apellidos", apellidoReserva));
             cmdInsertar.Parameters.Add(new SqlParameter("@correo", correoReserva));
             cmdInsertar.Parameters.Add(new SqlParameter("@tarjeta", tarjetaReserva));
-            cmdInsertar.Parameters.Add(new SqlParameter("@numero", numero));                    
-
+            cmdInsertar.Parameters.Add(new SqlParameter("@numero", numero));
             cmdInsertar.Connection.Open();
             bool res = Convert.ToBoolean(cmdInsertar.ExecuteNonQuery());
             cmdInsertar.Connection.Close();
-
             return res;
-        }
+        }//Fin de la funcion RealizarReserva.
 
-        public List<string> obtenerDatosHabitacion(int idHabitacion) {
+        public List<string> obtenerDatosHabitacion(int idHabitacion)
+        {
             SqlConnection connection = new SqlConnection(this.connString);
             String sqlSelect = "sp_Obtener_Datos_Habitacion";
             SqlDataAdapter sqlDataAdapterClient = new SqlDataAdapter();
@@ -47,19 +50,72 @@ namespace SistemaHotel.Models {
             DataRowCollection dataRow = dataSetPersonas.Tables["TSH_Habitacion"].Rows;
             List<string> habitacion = new List<string>();
 
-            foreach (DataRow currentRow in dataRow) {
+            foreach (DataRow currentRow in dataRow)
+            {
                 string numeroHab = currentRow["TN_Numero_Habitacion_TSH_Habitacion"].ToString();
                 string descripcion = currentRow["TC_Descripcion_TSH_Habitacion"].ToString(); ;
                 string imagen = currentRow["TI_Imagen_TSH_Habitacion"].ToString(); ;
-                string tarifa = currentRow["TN_Tarifa_TSH_Tipo_Habitacion"].ToString(); ;                
+                string tarifa = currentRow["TN_Tarifa_TSH_Tipo_Habitacion"].ToString(); ;
 
-                habitacion.Add(numeroHab);               
+                habitacion.Add(numeroHab);
                 habitacion.Add(descripcion);
                 habitacion.Add(imagen);
-                habitacion.Add(tarifa);                
-
-            }//foreach
+                habitacion.Add(tarifa);
+            }//Fin del foreach
             return habitacion;
-        }//obternerEstudiantes
-    }
-}
+        }//Fin de la funcion obtenerDatosHabitacion.
+
+        public List<TipoHabitacion> obtenerTiposHabitacion()
+        {
+            SqlConnection connection = new SqlConnection(this.connString);
+            String sqlSelect = "SP_ObtenerTipoHabitaciones";
+            SqlDataAdapter sqlDataAdapterClient = new SqlDataAdapter();
+            sqlDataAdapterClient.SelectCommand = new SqlCommand(sqlSelect, connection);
+            sqlDataAdapterClient.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+            DataSet dataSetPersonas = new DataSet();
+            sqlDataAdapterClient.Fill(dataSetPersonas, "TSH_Tipo_Habitacion");
+            sqlDataAdapterClient.SelectCommand.Connection.Close();
+
+            DataRowCollection dataRow = dataSetPersonas.Tables["TSH_Tipo_Habitacion"].Rows;
+            List<TipoHabitacion> tiposHabitaciones = new List<TipoHabitacion>();
+
+            foreach (DataRow currentRow in dataRow)
+            {
+                TipoHabitacion tipoTemp = new TipoHabitacion();
+                tipoTemp.Id = int.Parse(currentRow["TN_Identificador_TSH_Tipo_Habitacion"].ToString());
+                tipoTemp.Descripcion = currentRow["TC_Descripcion_TSH_Tipo_Habitacion"].ToString(); ;
+                tipoTemp.Tarifa = float.Parse(currentRow["TN_Tarifa_TSH_Tipo_Habitacion"].ToString()); ;
+                tipoTemp.CantidadPersonas = int.Parse(currentRow["TN_Cantidad_Personas_TSH_Tipo_Habitacion"].ToString()); ;
+                tiposHabitaciones.Add(tipoTemp);
+            }//Fin del foreach
+            return tiposHabitaciones;
+        }//Fin de la funcion obtenerDatosHabitacion.
+
+        public List<Habitacion> consultarDisponibilidad(int idTipoHabitacion)
+        {
+            SqlConnection connection = new SqlConnection(this.connString);
+            String sqlSelect = "SP_ConsultarDisponibilidad";
+            SqlDataAdapter sqlDataAdapterClient = new SqlDataAdapter();
+            sqlDataAdapterClient.SelectCommand = new SqlCommand(sqlSelect, connection);
+            sqlDataAdapterClient.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            sqlDataAdapterClient.SelectCommand.Parameters.Add(new SqlParameter("@idTipoHabitacion", idTipoHabitacion));
+
+            DataSet dataSetPersonas = new DataSet();
+            sqlDataAdapterClient.Fill(dataSetPersonas, "TSH_Habitacion");
+            sqlDataAdapterClient.SelectCommand.Connection.Close();
+
+            DataRowCollection dataRow = dataSetPersonas.Tables["TSH_Habitacion"].Rows;
+            List<Habitacion> habitaciones = new List<Habitacion>();
+
+            foreach (DataRow currentRow in dataRow)
+            {
+                Habitacion habitacionTemp = new Habitacion();
+                habitacionTemp.Id = int.Parse(currentRow["TN_Identificador_TSH_Habitacion"].ToString());
+                habitaciones.Add(habitacionTemp);
+            }//Fin del foreach
+            return habitaciones;
+        }//Fin de la función consultarDisponibilidad.
+
+    }//Fin de la clase ReservaModel.
+}//Fin del namespace.
